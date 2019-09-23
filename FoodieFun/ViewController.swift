94319951a6c4dba1affe4c07f81439760360e4dd
Steven Leyva <Sleyva1991@outlook.com>
2019-09-23 19:25:04 -0400

    Initial Commit

---
+++ FoodieFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodieFun
+//
+//  Created by Steven Leyva on 9/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
